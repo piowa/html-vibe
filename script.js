f8bef27c801d39bf0ec93028a266af5ee477ef0d
Piotr <www@wachala.pl>
2025-12-03 09:54:29 +0100

    Poprawki w layoutcie - menu mobile

--- script.js
+++ script.js
@@ -61,3 +61,25 @@ document.addEventListener('visibilitychange', () => {
     startInterval();
   }
 });
+
+
+// --- Mobilne menu hamburgerowe (dodane) ---
+const navToggle = document.querySelector('.nav-toggle');
+const mainNav = document.querySelector('.main-nav');
+
+if (navToggle && mainNav) {
+  navToggle.addEventListener('click', () => {
+    const isOpen = mainNav.classList.toggle('open');
+    navToggle.classList.toggle('open', isOpen);
+    navToggle.setAttribute('aria-expanded', isOpen ? 'true' : 'false');
+  });
+
+  mainNav.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+      mainNav.classList.remove('open');
+      navToggle.classList.remove('open');
+      navToggle.setAttribute('aria-expanded', 'false');
+    });
+  });
+}
+
